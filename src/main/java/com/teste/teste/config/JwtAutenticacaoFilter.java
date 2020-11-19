b66e2ffcf2562f1629d2cd02453834d770b595a7
comptialinux <comptialinux@comptialinux.casa>
2020-11-19 18:01:41 -0300

    Ajustando configurações de segurança

--- src/main/java/com/teste/teste/config/JwtAutenticacaoFilter.java
+++ src/main/java/com/teste/teste/config/JwtAutenticacaoFilter.java
@@ -1,6 +1,28 @@
 package com.teste.teste.config;
 
-public class JwtAutenticacaoFilter {
+import java.io.IOException;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class JwtAutenticacaoFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+     
+                Authentication autenticacao = GeradorTokenAutenticacao.getAuthentication((HttpServletRequest)request);
+
+                SecurityContextHolder.getContext().setAuthentication(autenticacao);
+
+                chain.doFilter(request, response);
+    }
     
 }
--- src/main/java/com/teste/teste/config/SegurancaConfig.java
+++ src/main/java/com/teste/teste/config/SegurancaConfig.java
@@ -1,7 +1,9 @@
 package com.teste.teste.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,11 +13,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SegurancaConfig extends WebSecurityConfigurerAdapter{
 
-    // @Override
-    // protected void configure(HttpSecurity http) throws Exception {
-    //     http.authorizeRequests()
-    //         .anyRequest().authenticated();
-    // }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests().antMatchers("v2/api-docs","/swagger*/**").permitAll()           
+            .and()            
+            .authorizeRequests().anyRequest().authenticated() 
+            .and()
+            .httpBasic();
+    }
     
 
     @Override
