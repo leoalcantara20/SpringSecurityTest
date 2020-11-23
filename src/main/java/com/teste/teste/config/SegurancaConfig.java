fc5c0674b9b6066ea6b3cb7793390a8b39647925
comptialinux <comptialinux@comptialinux.casa>
2020-11-23 09:23:22 -0300

    Adicionado código comentado para pesquisa em LDAP

--- src/main/java/com/teste/teste/config/SegurancaConfig.java
+++ src/main/java/com/teste/teste/config/SegurancaConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -43,5 +44,16 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter{
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {        
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("123456")).roles("USER");
+
+
+        // auth.ldapAuthentication()
+        //     .contextSource().url("ldap://algo.com:33389/dc=algo,dc=com")
+        //     .userSearchBase("ou=people")
+        //     .userSearchFilter("uid={0}")
+        //     .groupSearchBase("ou=groups")
+        //     .groupSearchFilter("member={0}")
+        //     .passwordCompare()
+        //     .passwordEncoder(new BCryptPasswordEncoder());            
+
     }
 }
